af59c9ea5126064bf13b72ce1b1182892c1ebe29
Spengs <Jotto465@gmail.com>
2017-02-26 22:46:37 -0600

    modal

--- server/public/scripts/controllers/modalController.js
+++ server/public/scripts/controllers/modalController.js
@@ -12,6 +12,19 @@ myApp.controller('ModalController', ['$scope', 'CoffeeFactory', '$uibModal',
     {type: 'cone', display: 'Cone'},
     {type: 'frenchpress', display: 'French Press'},
   ];
+  $scope.selectedBrewMethod = '';
+  $scope.brewStatus = false;
+  $scope.showBrewInfo = function () {
+    console.log(this);
+    console.log(this.method.type);
+    $scope.selectedBrewMethod = this.method.type;
+    console.log($scope.selectedBrewMethod);
+    if ($scope.brewStatus === false) {
+      $scope.brewStatus = true;
+    } else {
+      $scope.brewStatus = false;
+    }
+  };
   console.log(beans);
   $scope.closeModal = function () {
        $uibModalInstance.dismiss();
